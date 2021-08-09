dea9614a84a9d2b77cb7dbc47d61039907e7ead8
Jeff S <jeffreesun@protonmail.com>
2021-08-09 17:15:59 +0000

    Improve directory layout

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # urtypes
+
+Python implementation of the [Blockchain Commons UR Types specification](https://github.com/BlockchainCommons/Research/blob/master/papers/bcr-2020-006-urtypes.md)
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="urtypes",
+    version="0.1.0",
+    license="MIT license",
+    url="https://github.com/jreesun/urtypes",
+    description="Python implementation of the Blockchain Commons UR Types",
+    author="Jeff S",
+    author_email="jeffreesun@protonmail.com",
+    packages=find_namespace_packages("src", include=["*"]),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
